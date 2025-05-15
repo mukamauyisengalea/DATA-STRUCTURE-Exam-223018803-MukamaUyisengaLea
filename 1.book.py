9b6dd565ca7fa2b1da1278537cf3e2190bc3563e
mukamauyisengalea <mukamaleauyisenga@gmail.com>
2025-05-15 07:39:32 -0700

    organizing file into project name

--- 1.book.py
+++
@@ -1,9 +0,0 @@
-class Book:
-    def __init__(self, isbn, title, author, availability=True):
-        self.isbn = isbn
-        self.title = title
-        self.author = author
-        self.availability = availability
-
-    def __str__(self):
-        return f"{self.title} by {self.author} (ISBN: {self.isbn}, Available: {self.availability})"
